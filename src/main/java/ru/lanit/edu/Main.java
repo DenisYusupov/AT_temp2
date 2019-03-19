045e0f273981b6d2b08af97b67c981f54ef765a1
Denis <ivt_po@mail.ru>
2019-03-19 19:52:29 +0300

    initial commit add web el

--- src/main/java/ru/lanit/edu/Main.java
+++ src/main/java/ru/lanit/edu/Main.java
@@ -1,7 +1,11 @@
 package ru.lanit.edu;
 
 
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -9,9 +13,18 @@ public class Main {
     public static void main(String[] args) {
     System.setProperty("webdriver.chrome.driver","E:\\ch_driver\\chromedriver.exe") ;
 
-    WebDriver driver1 = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
 
-    driver1.get("https:\\ya.ru") ;
+    driver.get("https:\\ya.ru") ;
+    WebElement input = driver.findElement(By.id("text")) ;
+
+   // WebElement inputBtn = driver1.findElement(By.id("Text")) ;
+
+    //input.setKeys("sdfsdf");
+    input.sendKeys("asdasd");
+    input.sendKeys(Keys.ENTER);
+
+
 
 
 
