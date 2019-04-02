29866c0b094eaf9664daff424fe65c30d0161124
Denis <ivt_po@mail.ru>
2019-04-02 19:16:45 +0300

    page

---
+++ src/test/java/stepDefs.java
@@ -0,0 +1,16 @@
+import cucumber.api.java.en.Given;
+
+public class stepDefs {
+
+@Given("^Yande main Page is opended$")
+public void openYaMainPage(){
+    }
+
+@Given("^Search for '(.*)' $")// кол-во групп завхвата = кол-ву аргументов
+public void searchFor(String q){
+
+
+    }
+
+
+}
---
+++ src/test/resources/sample.feature
@@ -0,0 +1,3 @@
+Feature: Sample
+
+Scinario: yandex searchTest
